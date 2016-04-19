9bb5957da2c8e65336c112ebeacf0a6943583789
i5ting <i5ting@126.com>
2016-04-19 22:45:53 +0800

    reafct

--- index.js
+++ index.js
@@ -16,18 +16,22 @@ var c = {
   line9 : /Transfer\/sec:\s+(\d+.\d+.)/
 }
 
-var r = {}
-
 
 module.exports = function (file) {
   var t = fs.readFileSync('./wrk.log').toString()
+  var r = {}
+  
+  // r.time = t.match(c.line1)[1];
+  _set(t, r, 'line1', 'time', 1)
+  // r.host = t.match(c.line1)[2];
+  _set(t, r, 'line1', 'host', 2)
+  // r.port = t.match(c.line1)[3];
+  _set(t, r, 'line1', 'port', 3)
 
-  r.time = t.match(c.line1)[1];
-  r.host = t.match(c.line1)[2];
-  r.port = t.match(c.line1)[3];
-
-  r.threads = t.match(c.line2)[1]
-  r.connections = t.match(c.line2)[2]
+  // r.threads = t.match(c.line2)[1]
+  _set(t, r, 'line2', 'threads', 3)
+  // r.connections = t.match(c.line2)[2]
+  _set(t, r, 'line2', 'connections', 3)
 
   // Thread Stats   Avg      Stdev     Max   +/- Stdev
   // Latency   147.35ms   50.36ms 440.07ms   82.79%
@@ -38,29 +42,47 @@ module.exports = function (file) {
   r.latency_precent = t.match(c.line4)[4]
 
   // console.log( t.match(c.line5) )
-  r.req_per_second_avg = t.match(c.line5)[1]
-  r.req_per_second_stdev = t.match(c.line5)[2]
-  r.req_per_second_max = t.match(c.line5)[3]
-  r.req_per_second_precent = t.match(c.line5)[4]
+  // r.req_per_second_avg = t.match(c.line5)[1]
+  _set(t, r, 'line5', 'req_per_second_avg', 1)
+  // r.req_per_second_stdev = t.match(c.line5)[2]
+  _set(t, r, 'line5', 'req_per_second_stdev', 2)
+  // r.req_per_second_max = t.match(c.line5)[3]
+  _set(t, r, 'line5', 'req_per_second_max', 3)
+  // r.req_per_second_precent = t.match(c.line5)[4]
+  _set(t, r, 'line5', 'req_per_second_precent', 4)
 
   // 11845 requests in 2.07s, 1.71MB read
-  r.requests_count = t.match(c.line6)[1]
-  r.requests_seconds = t.match(c.line6)[2]
-  r.requests_read = t.match(c.line6)[3]
+  // r.requests_count = t.match(c.line6)[1]
+  _set(t, r, 'line6', 'requests_count', 1)
+  // r.requests_seconds = t.match(c.line6)[2]
+  _set(t, r, 'line6', 'requests_seconds', 2)
+  // r.requests_read = t.match(c.line6)[3]
+  _set(t, r, 'line6', 'requests_read', 3)
 
   //Socket errors: connect 0, read 257, write 2, timeout 0
   // console.log( t.match(c.line7) )
-  r.connect = t.match(c.line7)[1]
-  r.read = t.match(c.line7)[2]
-  r.write = t.match(c.line7)[3]
-  r.timeout = t.match(c.line7)[4]
+  // r.connect = t.match(c.line7)[1]
+  _set(t, r, 'line7', 'connect', 1)
+  // r.read = t.match(c.line7)[2]
+  _set(t, r, 'line7', 'read', 2)
+  // r.write = t.match(c.line7)[3]
+  _set(t, r, 'line7', 'write', 3)
+  // r.timeout = t.match(c.line7)[4]
+  _set(t, r, 'line7', 'timeout', 4)
+  
+  // r.requests_per_sec = t.match(c.line8)[1]
+  _set(t, r, 'line8', 'requests_per_sec', 1)
 
-  r.requests_per_sec = t.match(c.line8)[1]
-  r.transfer_per_sec = t.match(c.line9)[1]
+  // r.transfer_per_sec = t.match(c.line9)[1]
+  _set(t,r,'line9','transfer_per_sec',1)
 
   for(var k in r){
     console.log( k +' = ' + r[k] )
   }
   
   return r;
+}
+
+function _set(t,r,line1,key,index) {
+  r[key] = t.match(c[line1])[index];
 }
